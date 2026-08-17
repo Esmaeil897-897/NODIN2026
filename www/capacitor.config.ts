7949b042f72b560f1824588f10b99472bc3341ae
Esmaeil897-897 <Esmaeil.nodin@gmail.com>
2026-08-17 23:29:20 +0100

    Add capacitor.config.ts for Capacitor configuration

---
+++ www/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nodin.fleetstreetenglish',
+  appName: 'Fleet Street English',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
